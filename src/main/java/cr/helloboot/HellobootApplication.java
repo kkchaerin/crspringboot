30142621b6129c84d49cba2fb116edeb98d28fca
chaerin <krin0525@gmail.com>
2023-08-06 01:22:06 +0900

    feat: 요청값 사용하기

--- src/main/java/cr/helloboot/HellobootApplication.java
+++ src/main/java/cr/helloboot/HellobootApplication.java
@@ -21,10 +21,12 @@ public class HellobootApplication {
             servletContext.addServlet("hello", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    String name = req.getParameter("name");
+
                     // 웹 응답의 세가지 요소 : status, header(content-type), body
                     resp.setStatus(HttpStatus.OK.value());
                     resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                    resp.getWriter().println("Hello Servlet");
+                    resp.getWriter().println("Hello " + name);
                 }
             }).addMapping("/hello");
         });
