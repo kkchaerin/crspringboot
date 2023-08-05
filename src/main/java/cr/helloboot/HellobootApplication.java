4368ddc3ad31d5a3edfd5ce6f9db755bc664422f
chaerin <krin0525@gmail.com>
2023-08-05 23:07:59 +0900

    servlet 직접 작성해보기

---
+++ src/main/java/cr/helloboot/HellobootApplication.java
@@ -0,0 +1,33 @@
+package cr.helloboot;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HellobootApplication {
+
+    public static void main(String[] args) {
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    // 웹 응답의 세가지 요소
+                    resp.setStatus(200);
+                    resp.setHeader("Content-Type", "text/plain");
+                    resp.getWriter().println("Hello Servlet");
+                }
+            }).addMapping("/hello");
+        });
+        webServer.start(); //404가 뜬다는 것은 tomcat 이 잘 올라갔다는 뜻이다.
+
+
+    }
+
+}
