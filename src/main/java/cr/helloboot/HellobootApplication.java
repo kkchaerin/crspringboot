939655683ba5a83e099e0eb121e7a90002eecd5e
chaerin <krin0525@gmail.com>
2023-08-06 01:20:19 +0900

    fix: enum 적용

--- src/main/java/cr/helloboot/HellobootApplication.java
+++ src/main/java/cr/helloboot/HellobootApplication.java
@@ -3,6 +3,9 @@ package cr.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -18,9 +21,9 @@ public class HellobootApplication {
             servletContext.addServlet("hello", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    // 웹 응답의 세가지 요소
-                    resp.setStatus(200);
-                    resp.setHeader("Content-Type", "text/plain");
+                    // 웹 응답의 세가지 요소 : status, header(content-type), body
+                    resp.setStatus(HttpStatus.OK.value());
+                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
                     resp.getWriter().println("Hello Servlet");
                 }
             }).addMapping("/hello");
