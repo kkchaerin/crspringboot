e09d40266db245460c51b218f09dc7da32a22460
chaerin <krin0525@naver.com>
2023-08-13 23:23:56 +0900

    refactor: SpringBootApplication

--- src/main/java/cr/helloboot/HellobootApplication.java
+++ src/main/java/cr/helloboot/HellobootApplication.java
@@ -1,12 +1,11 @@
 package cr.helloboot;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
@@ -24,6 +23,6 @@ public class HellobootApplication {
 
     public static void main(String[] args) {
         // spring container : 의존 Object 를 삽입해준다.(DI)
-        MySpringApplication.run(HellobootApplication.class, args);
+        SpringApplication.run(HellobootApplication.class, args);
     }
 }
