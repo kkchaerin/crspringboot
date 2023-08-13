b35041e03cd0cdc5bfcb612ea87eda04d7de542c
chaerin <krin0525@naver.com>
2023-08-13 21:12:26 +0900

    test: dispatcherServlet으로 api 동작하게 하기

--- src/main/java/cr/helloboot/HelloController.java
+++ src/main/java/cr/helloboot/HelloController.java
@@ -1,7 +1,12 @@
 package cr.helloboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
+@RequestMapping("/hello")
 public class HelloController {
     private final HelloService helloService;
 
@@ -9,6 +14,8 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping
+    @ResponseBody //@ResponseBody는 @RestController 로 지정하면 자동으로 지정된다. 그래서 기존 restController 에서는 이 어노테이션이 없어도 동작했던 것.
     public String hello(String name){
         return helloService.sayHello(Objects.requireNonNull(name));
     }
