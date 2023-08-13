00347438560bec0b34fc2ba50702e53374c93150
chaerin <krin0525@naver.com>
2023-08-13 21:44:42 +0900

    test: bean factory method 대신 간결하게 bean 등록하기

--- src/main/java/cr/helloboot/HelloController.java
+++ src/main/java/cr/helloboot/HelloController.java
@@ -1,14 +1,13 @@
 package cr.helloboot;
 
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
-@RequestMapping("/hello")
-@Component
+@RestController
 public class HelloController {
     private final HelloService helloService;
 
@@ -16,8 +15,7 @@ public class HelloController {
         this.helloService = helloService;
     }
 
-    @GetMapping
-    @ResponseBody //@ResponseBody는 @RestController 로 지정하면 자동으로 지정된다. 그래서 기존 restController 에서는 이 어노테이션이 없어도 동작했던 것.
+    @GetMapping("/hello")
     public String hello(String name){
         return helloService.sayHello(Objects.requireNonNull(name));
     }
