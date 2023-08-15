c00bc7b82e7cc9e0289de43b61d2912a0cf60995
chaerin <krin0525@naver.com>
2023-08-15 20:40:07 +0900

    refactor: 롤백

--- src/test/java/cr/helloboot/HelloServiceTest.java
+++ src/test/java/cr/helloboot/HelloServiceTest.java
@@ -3,27 +3,8 @@ package cr.helloboot;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-@UnitTest
-@interface FastUnitTest{
-
-}
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
-@Test
-@interface UnitTest{
-
-}
-
 public class HelloServiceTest {
-    @FastUnitTest
+    @Test
     void simpleHelloService() {
         SimpleHelloService helloService = new SimpleHelloService();
 
