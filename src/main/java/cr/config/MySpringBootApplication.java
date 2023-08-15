563c187b221d998e0b5530e49e36db3ef566a889
chaerin <krin0525@naver.com>
2023-08-15 21:38:20 +0900

    feat: 자동 구성 정보 파일 분리

--- src/main/java/cr/config/MySpringBootApplication.java
+++ src/main/java/cr/config/MySpringBootApplication.java
@@ -1,5 +1,11 @@
 package cr.config;
 
+import cr.config.autoconfig.DispatcherServletConfig;
+import cr.config.autoconfig.TomcatWebServerConfig;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -7,6 +13,8 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE) // class, interface, enum 에 적용할 수 있다.
-@EnableMyAutoConfiguration
+@Configuration
+@ComponentScan
+@Import({DispatcherServletConfig.class, TomcatWebServerConfig.class})
 public @interface MySpringBootApplication {
 }
